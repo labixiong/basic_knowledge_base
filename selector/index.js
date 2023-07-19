8f968f1bc6cdfeff9a0276e5e4099d56cae9bf21
jintaiji@unicloud.com <jintaiji.pn@unicloud.com>
2023-07-19 18:03:36 +0800

    feat: 增加css选择器练

---
+++ selector/index.js
@@ -0,0 +1,11 @@
+const first = document.getElementsByTagName('div')[0]
+console.log(first, 'first');
+
+const second = document.getElementsByClassName('second')[0]
+console.log(second, 'second');
+
+const third = document.getElementById('third')
+console.log(third, 'third');
+
+const text = document.getElementsByName('five')[0]
+console.log(text, 'text');
